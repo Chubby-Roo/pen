a7cef5f611015621f2dce54040c2ae26bb963c71
Monochrome-Fox <toxicibu@icloud.com>
2017-05-26 04:34:15 -0700

    Base and refactor
    so base had some of the code from advanced.
    buuut now it's gone and it's literally the base.
    
    Also <penInstance>.html now takes another argument
    parse - will parse as html
    basically innerHTML adds a delay to load time
    this delay is caused by the html parser
    so by default it'll assume that you're referencing "textContent"
    instead of "innerHTML" but if you set this option to true
    then it'll use "innerHTML" and not "textContent"

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var Start, body, contextmenu, dropdown, head, header, imager, load;
+var Start, body, contextmenu, dropdown, head, header, imager, load, view;
 
 ({body, head} = document);
 
@@ -33,8 +33,8 @@ dropdown.fn = dropdown.prototype = {
 
 dropdown.prototype.addLink = function(name, link, desc = "no description") {
   var a, hr, min;
-  a = pen('<a class="dropdown-content-link" href="' + link + '">').html(name + '<br>');
-  min = pen('<span class="dropdown-content-link-location">').html(link + '<br>');
+  a = pen('<a class="dropdown-content-link" href="' + link + '">').html(name + '<br>', false, true);
+  min = pen('<span class="dropdown-content-link-location">').html(link + '<br>', false, true);
   min.appendTo(a);
   desc = pen('<span class="dropdown-content-link-location">').html(desc);
   desc.appendTo(a);
@@ -69,6 +69,106 @@ dropdown.prototype.css = dropdown.prototype.style = function(element = String, .
 dropdown.prototype.embedInto = function(element) {
   return this.button.el.outerHTML + this.content.el.outerHTML;
 };
+
+view = (function(window, document, pen) {
+  var Ghldnm;
+  Ghldnm = void 0;
+  view = function(holdername, views, classes) {
+    var cls, ele, i, j, len, name, prop, setHTML;
+    if (!(this instanceof view)) {
+      return new view(holdername, views, classes);
+    } else if (holdername instanceof view) {
+      for (prop in holdernamme) {
+        this[prop] = holdername[prop];
+      }
+    } else {
+      Ghldnm = this.__G_HOLDER_NAME = holdername;
+      this[holdername] = {};
+      this.__classes = {};
+      for (i = j = 0, len = views.length; j < len; i = ++j) {
+        view = views[i];
+        ({name, ele} = view);
+        if (type(ele) === 'string') {
+          ele = pen(ele);
+        }
+        if (view.setHTML != null) {
+          setHTML = view.setHTML;
+          ele.html(setHTML);
+        }
+        if (view.important != null) {
+          if (view.important === true) {
+            this.important = this.main = ele;
+          }
+        }
+        this[name] = ele;
+      }
+      for (cls in classes) {
+        this.__classes[cls] = classes[cls];
+      }
+    }
+    view.prototype[`remove${self.__G_HOLDER_NAME}`] = function(name, fully) {
+      var self;
+      self = this;
+      pen(self[self.__G_HOLDER_NAME][name]).remove();
+      if (fully === true) {
+        delete self[self.__G_HOLDER_NAME][name];
+      } else {
+        void 0;
+      }
+      return self;
+    };
+  };
+  view.fn = view.prototype = {
+    constructor: view
+  };
+  view.prototype.add = function(name, options, ohr = false) {
+    var evalCls, self, temp;
+    evalCls = function() {
+      if (options.extraClasses != null) {
+        return options.extraClasses.split(/\s+/gi).join(' ');
+      } else {
+        return void 0;
+      }
+    };
+    self = this;
+    temp = void 0;
+    if (ohr !== false) {
+      self[self.__G_HOLDER_NAME][name] = {};
+      self[self.__G_HOLDER_NAME][name].el = void 0;
+      self[self.__G_HOLDER_NAME][name].hr = pen(`<br class='${self.__classes.priorityName}-divider'>`);
+    }
+    if (options.event != null) {
+      temp = pen(`<span align='center' class='${self.__classes.main} ${self.__classes.type['button']} ${evalCls()}`).html(options.text).on("click", options.event);
+    } else if (options.link != null) {
+      temp = pen(`<span align='center' class='${self.__classes.main} ${self.__classes.type['link']} ${evalCls()}'`).html(options.text).href(options.link);
+    } else if (options.custom != null) {
+      temp = pen(`<sapn align='center' class='${self.__classes.main} custom ${evalCls()}'>`);
+    }
+    if (ohr !== false) {
+      self[self.___G_HOLDER_NAME][name].el = temp;
+    } else {
+      self[self.___G_HOLDER_NAME][name] = temp;
+    }
+    return self;
+  };
+  view.prototype.init = function() {
+    console.log("you'll need to create this yourself");
+  };
+  view.prototype.deploy = function(element) {
+    var self;
+    self = this;
+    pen(element).append(self.important);
+    return self;
+  };
+  if (typeof module !== "undefined" && module !== null) {
+    if (module.exports != null) {
+      module.exports = view;
+    } else {
+      window.view = view;
+    }
+  }
+  return view;
+})(window, document, pen);
 
 contextmenu = {
   commands: {},
@@ -143,7 +243,7 @@ header = {
     console.log(name);
     self = header;
     if (evhr instanceof dropdown) {
-      temp = pen('<span align="center" class="header-button dropdown Ril">').html(evhr.button.element.outerHTML + evhr.content.element.outerHTML);
+      temp = pen('<span align="center" class="header-button dropdown Ril">').html(evhr.button.element.outerHTML + evhr.content.element.outerHTML, false, true);
       self.buttons[name] = temp;
       return self;
     }
--- view.coffee
+++ view.coffee
@@ -1,41 +1,69 @@
-view = (holdername, views, classes) ->
-  if not (this instanceof view)
-    return new view(holdername, views, classes)
-  else if holdername instanceof view
-    for prop of holdernamme
-      @[prop] = holdername[prop]
-  else
-    @__G_HOLDER_NAME = holdername
-    @[holdername] = {}
-    @__classes = {}
-    for view, i in views
-      {name, ele} = view
-      if type(ele) is 'string'
-        ele = pen ele
-      if view.setHTML?
-        setHTML = view.setHTML
-        ele.html(setHTML)
-      if view.important?
-        if view.important is true
-          @important = @main = ele
-      @[name] = ele
-    for cls of classes
-      @__classes[cls] = classes[cls]
+((window, document, pen) ->
+  Ghldnm = undefined
+  view = (holdername, views, classes) ->
+    if not (this instanceof view)
+      return new view(holdername, views, classes)
+    else if holdername instanceof view
+      for prop of holdernamme
+        @[prop] = holdername[prop]
+    else
+      Ghldnm = @__G_HOLDER_NAME = holdername
+      @[holdername] = {}
+      @__classes = {}
+      for view, i in views
+        {name, ele} = view
+        if type(ele) is 'string'
+          ele = pen ele
+        if view.setHTML?
+          setHTML = view.setHTML
+          ele.html(setHTML)
+        if view.important?
+          if view.important is true
+            @important = @main = ele
+        @[name] = ele
+      for cls of classes
+        @__classes[cls] = classes[cls]
+    view::["remove#{self.__G_HOLDER_NAME}"] = (name, fully) ->
+      self = this
+      pen(self[self.__G_HOLDER_NAME][name]).remove()
+      if fully is true then delete self[self.__G_HOLDER_NAME][name] else undefined
+      return self
+    return
+
+  view.fn = view:: =
+    constructor: view
+
+  view::add = (name, options, ohr = false) ->
+    evalCls = () -> if options.extraClasses? then options.extraClasses.split(/\s+/gi).join(' ') else undefined
+    self = this
+    temp = undefined
+    if ohr isnt false
+      self[self.__G_HOLDER_NAME][name] = {}
+      self[self.__G_HOLDER_NAME][name].el = undefined
+      self[self.__G_HOLDER_NAME][name].hr = pen("<br class='#{self.__classes.priorityName}-divider'>")
+    if options.event?
+      temp = pen "<span align='center' class='#{self.__classes.main} #{self.__classes.type['button']} #{evalCls()}"
+      .html options.text
+      .on "click", options.event
+    else if options.link?
+      temp = pen "<span align='center' class='#{self.__classes.main} #{self.__classes.type['link']} #{evalCls()}'"
+      .html options.text
+      .href options.link
+    else if options.custom?
+      temp = pen "<sapn align='center' class='#{self.__classes.main} custom #{evalCls()}'>"
+    if ohr isnt false
+      self[self.___G_HOLDER_NAME][name].el = temp
+    else
+      self[self.___G_HOLDER_NAME][name] = temp
+    return self
+
+  view::init = () ->
+    console.log "you'll need to create this yourself"
+    return
+
+  view::deploy = (element) ->
+    self = this
+    pen(element).append(self.important)
+    return self
   return
-
-view.fn = view:: =
-  constructor: view
-  
-
-view::add = (name, options) ->
-  self = this
-  temp = undefined
-  if options.event?
-    temp = pen "<span align='center' class='#{self.__classes.main} #{self.__classes.type['button']}"
-  self[self.___G_HOLDER_NAME][name] = temp 
-  return this
-
-view::deploy = (element) ->
-  self = this
-  pen(element).append(self.important)
-  return this
+  )(window, document, pen)
